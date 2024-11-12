81d1d44661ecfdbe130773236da4dabff828cdb9
Andrey <AKuropatkin@egartech.ru>
2024-11-12 16:54:33 +0300

    refactor/code-and-styles

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
   app.enableShutdownHooks();
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:5173/', 'https://hr-automation-frontend-walb.vercel.app/'],
+    origin: [
+      'http://localhost:5173',
+      'https://hr-automation-frontend-walb.vercel.app',
+      'https://hr-automation-frontend-sigma.vercel.app',
+    ],
   });
 
   await app.listen(5500);
